a815bc9df8f5228575c5bde7e9340fd7f8d2ab76
Eun-Seo Seo <101198913+tjdmstj@users.noreply.github.com>
2023-05-16 03:09:11 +0900

    detail post & make reply components

--- hijob/src/Cummnity_post.jsx
+++ hijob/src/Cummnity_post.jsx
@@ -3,18 +3,27 @@ import MenuBar from "./components/MenuBar";
 import "./Cummunity_post.css";
 import { Link } from "react-router-dom";
 import arrow from "./APP/arrow-prev.png";
+import chat from "./APP/chat-writing.png";
+import CummunityDetailPost from "./components/CummunityDetailPost";
+import seventeen from "./APP/Seventeen.png";
+import heart from "./APP/heart.png";
 
 function Cummunity_post(){
     return(
         <>
-        <div className="App">
+        <div id="container">
              <heaer id="post_header">
                 <Link to ="/cummunity" id="box">
                  <img src={arrow} id="arrow"/>
                  <div id="cummunity_button">커뮤니티</div>
                 </Link>
              </heaer>
-             <div className="menu_bar">
+
+            <div id="cummunity_datail_post"> 
+                 <CummunityDetailPost/>
+            </div>
+            
+            <div className="menu_bar">
                 <MenuBar></MenuBar>
             </div>
         </div>
---
+++ hijob/src/components/CummunityDetailPost.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import "./CummunityDetailPost.css"
+import chat from "../APP/chat-gray.png";
+import seventeen from "../APP/Seventeen.png";
+import heart from "../APP/heart.png";
+import Reply from "./Reply"
+
+function CummunityDetailPost(){
+    const location =useLocation();
+    if(location.state){
+        const title = location.state.title;
+        const author = location.state.author;
+        const time = location.state.time;
+        const count = location.state.count;
+        const like = location.state.like;
+        const reply = location.state.reply;
+        
+        return(
+            <div id="detail_post_content">
+                <div className="datailpost_container">
+                    <div className="post_container">
+                        <div id="text_box">
+                            <h2>{title}</h2>
+                            <div><span id="cummunity_author">{author}</span> <span id="cummunity_time">{time} </span></div>
+                            <div><span id="cummunity_count">조회수 {count} </span><span id="cummunity_like">추천수 {like}</span></div>
+                        </div>
+                        <div id="reply_box">
+                            <h4>{reply}</h4>
+                            <span id="cummunity_reply">댓글</span>
+                        </div>
+                    </div> 
+                    <hr className="mainline"/>
+                </div>
+
+                <div> 
+                    <img src={seventeen}  id="post_image" alt="공고문 대표사진"/>
+                    <div id="detail_post">
+                    <p>Say, say, say, say영웅본색 like this시간과 공간에 구애받지 않는 자세힘을 다하고 쓰러져도포기를 모르고 날뛰는 중</p>
+                    <p>Say, say, say, say영웅본색 like this시간과 공간에 구애받지 않는 자세힘을 다하고 쓰러져도포기를 모르고 날뛰는 중</p>
+                    <p>Say, say, say, say영웅본색 like this시간과 공간에 구애받지 않는 자세힘을 다하고 쓰러져도포기를 모르고 날뛰는 중</p>
+                    <p>Say, say, say, say영웅본색 like this시간과 공간에 구애받지 않는 자세힘을 다하고 쓰러져도포기를 모르고 날뛰는 중</p>
+                    <p>Say, say, say, say영웅본색 like this시간과 공간에 구애받지 않는 자세힘을 다하고 쓰러져도포기를 모르고 날뛰는 중</p>
+                    </div>
+                    <hr className="mainline"/>
+                    <div id="like_container">
+                    <img src={heart} id="like_button" alt="좋아요"/>
+                    <span id="post_like">좋아요</span>
+                    </div>
+
+                    <form id="reply_container">
+                    <textarea name="reply" form="reply_container" placeholder="광고성 글, 비방글 등은 사전 동의없이 삭제될 수 있습니다."  id="reply_input" rows="9"></textarea>
+                    <hr className="mainline" id="post_line"/>
+                    <input type="submit" id="post_submit_button" value="등록"></input>
+                    </form>
+
+                    <div id="reply_count">
+                        <img src={chat} id="chat_icon" alt="채팅"/>
+                        <span id="post_chat">{reply}개의 댓글이 있습니다.</span>
+                    </div>
+                    <hr className="mainline"/>
+                    <div id="replies">
+                        <Reply name={"익명"} 
+                        contents={"Say, say, say, say영웅본색 like this시간과 공간에 구애받지 않는 자세힘을 다하고 쓰러져도포기를 모르고 날뛰는 중 Say, say, say, say영웅본색 like this시간과 공간에 구애받지 않는 자세힘을 다하고 쓰러져도포기를 모르고 날뛰는 중 Say, say, say, say영웅본색 like this시간과 공간"} 
+                        date={"2023.05.05"} time={"16:18"}/>
+                    </div>
+                    <hr className="mainline"/>
+                </div>
+            </div>
+    )
+    }else{
+        return null;
+    }
+}
+
+export default CummunityDetailPost;
