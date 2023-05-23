9c2a2858d5ab10ff1534c54998a8211eb5857c30
codms85 <lce0805@naver.com>
2023-05-24 00:10:25 +0900

    chaeeun_1

--- hijob/src/Home.jsx
+++ hijob/src/Home.jsx
@@ -1,11 +1,24 @@
 import React, { useDeferredValue } from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import axios from 'axios';
+
+
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 import './Home.css';
 import MenuBar from "./components/MenuBar";
 import SearchBar from "./components/SearchBar";
 import testimg from "./APP/testimage.png";
 import jjang from "./APP/jjang.png";
+import { Router } from "react-router-dom";
 
 function Home(){
+    
     return(
         <div className="body">
             <div className="search">
@@ -13,11 +26,23 @@ function Home(){
             </div>
     
             <div className="main_content">
-                <img src={testimg} alt="메인이미지" className="main_img"/>
+                <Swiper
+                    modules={[Navigation, Pagination]}
+                    spaceBetween={0} //슬라이스 간격
+                    slidesPerView={1} //슬라이스 개수
+                    //navigation
+                    pagination={{ clickable: false }}
+                    //</div>scrollbar={{ draggable: true }}
+                    >
+                    <SwiperSlide> <img src={testimg} alt="메인이미지" className="main_img"/> </SwiperSlide>
+                    <SwiperSlide> <img src={jjang} alt="메인이미지" className="main_img"/> </SwiperSlide>
+                    <SwiperSlide> <img src={testimg} alt="메인이미지" className="main_img"/> </SwiperSlide>
+                </Swiper>
+                
                 <h1>추천공고</h1>
                 <hr className="mainline"/>
                 <div className="box">
-                    <h2>영상제작 크루 모집</h2>
+                    <h2>영상제작 크류</h2>
                     <h3>마감일 2023.05.12.<br/>조회수 1234</h3>
                 </div>
                 <hr className="mainline"/>
@@ -41,12 +66,11 @@ function Home(){
                 </div>
             </div>
 
+
             <div className="menu_bar">
                 <MenuBar></MenuBar>
             </div>
-                
          
-
             <div className="lastblock"></div>
         </div>
     );
--- hijob/src/components/SearchBar.jsx
+++ hijob/src/components/SearchBar.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./SearchBar.css";
-import menu_b from "../APP/menu_black.png";
 import bell from "../APP/bell_black.png"; 
 import search from "../APP/search.png"; 
 
 function SearchBar(){
     return( 
         <div className="Sbar">
-            <img src={menu_b} alt="메뉴" className="top_icon_1"/>
             <img src={bell} alt="알림" className="top_icon_2"/>
             
             <div className="searchbox">
