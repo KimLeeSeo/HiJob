b60e84218138059fa58336c5a72f56906275a124
Eun-Seo Seo <101198913+tjdmstj@users.noreply.github.com>
2023-05-28 02:50:08 +0900

    home post완성~

--- hijob/src/Search_result.jsx
+++ hijob/src/Search_result.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Search_result.css'
 import heart from "./APP/heart.png";
 import redheart from "./APP/redheart.png";
+import { useLocation } from 'react-router-dom';
 
 import { useState } from "react";
 
@@ -9,7 +10,7 @@ import { useState } from "react";
 export default function Search_result() {
   const [imgSrc,setimgSrc]=useState(heart);
   const [isClick,setisClick]=useState(false);
-
+ const location = useLocation();
   const ChangeColor=()=>{
         if(isClick){
             setimgSrc(redheart);
@@ -20,22 +21,24 @@ export default function Search_result() {
         }
     }
 
-  return (
+    const title = location.state.title;
+    const content = location.state.content;
+    const [htmlString, setHtmlString] = useState(content);
+
+    return (
     <div className='search_body'>
       <div className='result_header'>
 
       </div>
       <div className='result_content'>
         <h1>
-          [네이버 웹툰] 웹툰작가 모집
+          {title}
         </h1>
         <hr/>
         <h2>
-          모집요강
+          [모집요강]
         </h2>
-        <h3>
-        Unforgiven I'm a villain I'm a Unforgiven 난 그 길을 걸어 Unforgiven I'm a villain I'm a 새 시대로 기억될 unforgiven Yeah what you want? 불편함이 깃든 face, wanna shut me up 사냥감을 거듭해 찾는 워리어S 너의 game에 난 문제아 such a freak 골칫거리 Let me tell you 'bout LE SSERAFIM 내가 제일 싫은 건 낡은 대물림 어둠 속 불을 켜 마치 rebellion We gonna kick it break it rules gon' give up Unforgiven yes I was bleeding 힘없이 늘 져야만 했던 싸움 but I ride 바란 적도 없어 용서 따위는 난 금기를 겨눠 watch me now Now now now ...
-        </h3>
+        <h3 dangerouslySetInnerHTML={{ __html: htmlString }} id="search_result_content"></h3>
       </div>
       
       <div className="bar_body">
@@ -49,4 +52,5 @@ export default function Search_result() {
       </div>
     </div>
   )
+
 }
